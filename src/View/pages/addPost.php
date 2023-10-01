6d404bd90bd4763a108076bd82326b0b32845296
RafaelSoares12 <soares.lopes@mail.uft.edu.br>
2023-10-01 12:56:25 -0300

    fix: funcionalidade de add post

--- src/View/pages/addPost.php
+++ src/View/pages/addPost.php
@@ -1,35 +1,112 @@
+<?php
+session_start(); // Inicie a sessão
+
+// Limpar o buffer de redirecionamento
+ob_start();
+
+// Fazer conexão com o banco de dados
+include_once '../../config/connection.php';
+
+// Verificar se o formulário foi enviado
+if (isset($_POST['SubmitPost'])) {
+    // Obtém os dados do formulário
+    $titulo = $_POST['titulo'];
+    $conteudo = $_POST['conteudo'];
+    $autor_id = $_POST['autor_id'];
+    $assunto = $_POST['assunto'];
+    $slug = $_POST['slug'];
+
+    // Faça a validação dos dados, se necessário
+
+    // Crie uma instância de Connection
+    $conexao = new Connection();
+    $pdo = $conexao->getConnection();
+
+    // Prepare e execute a inserção no banco de dados
+    $sql = "INSERT INTO postagens (titulo, conteudo, autor_id, assunto, slug) VALUES (:titulo, :conteudo, :autor_id, :assunto, :slug)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':titulo', $titulo);
+    $stmt->bindParam(':conteudo', $conteudo);
+    $stmt->bindParam(':autor_id', $autor_id);
+    $stmt->bindParam(':assunto', $assunto);
+    $stmt->bindParam(':slug', $slug);
+    
+    if ($stmt->execute()) {
+        $_SESSION['msg'] = "<p style='color: #008000;'>Postagem adicionada com sucesso!</p>";
+    } else {
+        $_SESSION['msg'] = "<p style='color: #f00;'>Erro ao adicionar postagem.</p>";
+    }
+    
+    // Redirecione de volta à página de adição de postagem
+    header("Location: addPost.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Postagem</title>
+    
+    <link rel="stylesheet" href="../css/addPost.css">
+
 </head>
 <body>
-    <h1>Adicionar Nova Postagem</h1>
 
-    <form method="POST" action="../../controller/PostController.php">
-    <label for="titulo">Título:</label>
-        <input type="text" id="titulo" name="titulo" required>
-        <br><br>
+<?php
+if (isset($_SESSION['msg'])) {
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']); // Limpa a mensagem após exibi-la
+}
+?>
 
-        <label for="conteudo">Conteúdo:</label>
-        <textarea id="conteudo" name="conteudo" required></textarea>
-        <br><br>
+<div class="box">
+    <form action="addPost.php" method="post">
+        <fieldset>
+            <legend><b>Adicionar Nova Postagem</b></legend>
+            <br>
 
-        <label for="autor_id">ID do Autor:</label>
-        <input type="number" id="autor_id" name="autor_id" required>
-        <br><br>
+            <!-- Título -->
+            <div class="InputBox">
+                <input type="text" name="titulo" id="titulo" class="InputUser" required>
+                <label for="titulo" class="labelInput">Título</label>
+            </div>
+            <br><br>
 
-        <label for="assunto">Assunto:</label>
-        <input type="text" id="assunto" name="assunto" required>
-        <br><br>
+            <!-- Conteúdo -->
+            <div class="InputBox">
+                <textarea name="conteudo" id="conteudo" class="InputUser" required></textarea>
+                <label for="conteudo" class="labelInput">Conteúdo</label>
+            </div>  
+            <br><br>
 
-        <label for="slug">Slug:</label>
-        <input type="text" id="slug" name="slug" required>
-        <br><br>
+            <!-- Autor ID -->
+            <div class="InputBox">
+                <input type="number" name="autor_id" id="autor_id" class="InputUser" required>
+                <label for="autor_id" class="labelInput">ID do Autor</label>
+            </div>
+            <br><br>
 
-        <input type="submit" value="Adicionar Postagem">
+            <!-- Assunto -->
+            <div class="InputBox">
+                <input type="text" name="assunto" id="assunto" class="InputUser" required>
+                <label for="assunto" class="labelInput">Assunto</label>
+            </div>
+            <br><br>
+
+            <!-- Slug -->
+            <div class="InputBox">
+                <input type="text" name="slug" id="slug" class="InputUser" required>
+                <label for="slug" class="labelInput">Slug</label>
+            </div>
+            <br><br>
+
+            <input type="submit" name="SubmitPost" id="SubmitPost"><br><br>
+        </fieldset>
     </form>
+</div>
+
 </body>
 </html>

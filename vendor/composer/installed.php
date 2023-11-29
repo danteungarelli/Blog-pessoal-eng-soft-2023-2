c230650b1f6bdddf3ef54a258980029306c10a5c
Daniel Noleto <daniel.noleto@mail.uft.edu.br>
2023-11-29 01:37:32 -0300

    Adicionando o teste unitário de contar seguidores

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'fac7da280f5eb3c071b6e219fc6dcd940112f28c',
+        'reference' => 'c08c0ad3f3807641c34a9405a16f9022c5ba791e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'fac7da280f5eb3c071b6e219fc6dcd940112f28c',
+            'reference' => 'c08c0ad3f3807641c34a9405a16f9022c5ba791e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
